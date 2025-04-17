2f2aa78cad73d4c52716a311899463366b292e55
CiaOlsson <146974056+CiaOlsson@users.noreply.github.com>
2025-04-17 11:51:08 +0200

    Migrating database

--- NutritionTracker/NutritionTracker.Domain/DomainEntities/DomainEvent.cs
+++ NutritionTracker/NutritionTracker.Domain/DomainEntities/DomainEvent.cs
@@ -7,13 +7,12 @@ using System.Threading.Tasks;
 namespace NutritionTracker.Domain.DomainEntities
 {
     public class DomainEvent
-	{
-            
+	{ 
         public Guid Id { get; set; }
+		public int Version { get; set; }
 		public string EventType { get; set; }
 		public string Data { get; set; } 
 		public DateTime Timestamp { get; set; }
 		public Guid UserId { get; set; }
-	
 	}
 }
---
+++ NutritionTracker/NutritionTracker.Infrastructure/Migrations/20250417094758_AddEventVersion.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NutritionTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEventVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "Events");
+        }
+    }
+}
