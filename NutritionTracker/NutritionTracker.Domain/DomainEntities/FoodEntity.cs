c16f91e85b1e0bb77cc5844d550e1e463a79539d
CiaOlsson <146974056+CiaOlsson@users.noreply.github.com>
2025-04-28 18:37:20 +0200

    removed unneccesary files, and uppdated column names in script and FoodEntity for readability also fixed so FoodId is PrimaryKey

--- NutritionTracker/NutritionTracker.Domain/DomainEntities/FoodEntity.cs
+++ NutritionTracker/NutritionTracker.Domain/DomainEntities/FoodEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,66 +10,67 @@ namespace NutritionTracker.Domain.DomainEntities
 	public class FoodEntity
 	{
 		//Jag tar med alla värden tillsvidare. Kanske skalar ner senare. 
+		[Key]
 		public int FoodId { get; set; }
 		public string Name { get; set; }
 		public string Group { get; set; }
-		public float? EnergyKcal { get; set; }
-		public float? EnergyKj { get; set; }
-		public float? FatTotalG { get; set; }
-		public float? ProteinG { get; set; }
-		public float? CarbohydratesG { get; set; }
-		public float? FiberG { get; set; }
-		public float? WaterG { get; set; }
-		public float? AlcoholG { get; set; }
-		public float? AshG { get; set; }
-		public float? SugarsTotalG { get; set; }
-		public float? MonosaccharidesG { get; set; }
-		public float? DisaccharidesG { get; set; }
-		public float? AddedSugarG { get; set; }
-		public float? FreeSugarG { get; set; }
-		public float? WholeGrainTotalG { get; set; }
-		public float? SaturatedFattyAcidsG { get; set; }
-		public float? FattyAcids410G { get; set; }
-		public float? LauricAcidG { get; set; }
-		public float? MyristicAcidG { get; set; }
-		public float? PalmiticAcidG { get; set; }
-		public float? StearicAcidG { get; set; }
-		public float? ArachidicAcidG { get; set; }
-		public float? MonounsaturatedFattyAcidsG { get; set; }
-		public float? PalmitoleicAcidG { get; set; }
-		public float? OleicAcidG { get; set; }
-		public float? PolyunsaturatedFattyAcidsG { get; set; }
-		public float? LinoleicAcidG { get; set; }
-		public float? LinolenicAcidG { get; set; }
-		public float? ArachidonicAcidG { get; set; }
-		public float? EPAg { get; set; }
-		public float? DPAg { get; set; }
-		public float? DHAg { get; set; }
-		public float? CholesterolMg { get; set; }
-		public float? VitaminAReUg { get; set; }
-		public float? RetinolUg { get; set; }
-		public float? BetaCaroteneUg { get; set; }
-		public float? VitaminDUg { get; set; }
-		public float? VitaminEMg { get; set; }
-		public float? VitaminKug { get; set; }
-		public float? ThiaminMg { get; set; }
-		public float? RiboflavinMg { get; set; }
-		public float? NiacinMg { get; set; }
-		public float? NiacinEquivalentsMg { get; set; }
-		public float? VitaminB6Mg { get; set; }
-		public float? FolateTotalUg { get; set; }
-		public float? VitaminB12Ug { get; set; }
-		public float? VitaminCMg { get; set; }
-		public float? PhosphorusMg { get; set; }
-		public float? IodineUg { get; set; }
-		public float? IronMg { get; set; }
-		public float? CalciumMg { get; set; }
-		public float? PotassiumMg { get; set; }
-		public float? MagnesiumMg { get; set; }
-		public float? SodiumMg { get; set; }
-		public float? SaltG { get; set; }
-		public float? SeleniumUg { get; set; }
-		public float? ZincMg { get; set; }
-		public float? WastePercent { get; set; }
+		public int? Energy_kcal { get; set; }
+		public int? Energy_kj { get; set; }
+		public float? FatTotal_g { get; set; }
+		public float? Protein_g { get; set; }
+		public float? Carbohydrates_g { get; set; }
+		public float? Fiber_g { get; set; }
+		public float? Water_g { get; set; }
+		public float? Alcohol_g { get; set; }
+		public float? Ash_g { get; set; }
+		public float? SugarsTotal_g { get; set; }
+		public float? Monosaccharides_g { get; set; }
+		public float? Disaccharides_g { get; set; }
+		public int? AddedSugar_g { get; set; }
+		public int? FreeSugar_g { get; set; }
+		public int? WholeGrainTotal_g { get; set; }
+		public float? SaturatedFattyAcids_g { get; set; }
+		public float? FattyAcids410_g { get; set; }
+		public float? LauricAcid_g { get; set; }
+		public float? MyristicAcid_g { get; set; }
+		public float? PalmiticAcid_g { get; set; }
+		public float? StearicAcid_g { get; set; }
+		public float? ArachidicAcid_g { get; set; }
+		public float? MonounsaturatedFattyAcids_g { get; set; }
+		public float? PalmitoleicAcid_g { get; set; }
+		public float? OleicAcid_g { get; set; }
+		public float? PolyunsaturatedFattyAcids_g { get; set; }
+		public float? LinoleicAcid_g { get; set; }
+		public float? LinolenicAcid_g { get; set; }
+		public float? ArachidonicAcid_g { get; set; }
+		public float? EPA_g { get; set; }
+		public float? DPA_g { get; set; }
+		public float? DHA_g { get; set; }
+		public float? Cholesterol_mg { get; set; }
+		public float? Vitamin_A_Re_ug { get; set; }
+		public float? Retinol_ug { get; set; }
+		public float? BetaCarotene_ug { get; set; }
+		public float? Vitamin_D_ug { get; set; }
+		public float? Vitamin_E_mg { get; set; }
+		public float? Vitamin_K_ug { get; set; }
+		public float? Thiamin_mg { get; set; }
+		public float? Riboflavin_mg { get; set; }
+		public float? Niacin_mg { get; set; }
+		public float? NiacinEquivalents_mg { get; set; }
+		public float? Vitamin_B6_mg { get; set; }
+		public float? FolateTotal_ug { get; set; }
+		public float? Vitamin_B12_ug { get; set; }
+		public float? Vitamin_C_mg { get; set; }
+		public int? Phosphorus_mg { get; set; }
+		public float? Iodine_ug { get; set; }
+		public float? Iron_mg { get; set; }
+		public int? Calcium_mg { get; set; }
+		public int? Potassium_mg { get; set; }
+		public int? Magnesium_mg { get; set; }
+		public int? Sodium_mg { get; set; }
+		public float? Salt_g { get; set; }
+		public float? Selenium_ug { get; set; }
+		public float? Zinc_mg { get; set; }
+		public float? Waste_percent { get; set; }
 	}
 }
